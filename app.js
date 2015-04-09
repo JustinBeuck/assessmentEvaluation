5014269ff0d2d49764f30ad898ba29bc817ce8ee
Justin Beuck <jbeuck@yahoo.com>
2015-04-09 13:12:05 -0500

    changed app.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express'); 
 var app = express(); 
 
-app.use(express.static(__dirname + '/public')); 
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-app.listen(process.env.PORT || 3000);
-console.log('listening on port 3000...');
+var port = process.env.PORT || 8080;
+
+// set the view engine to html
+app.set('view engine', 'html');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// html render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
